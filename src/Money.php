fad9be25214df4a5ce3e065ef7a66371c4f9b761
Patrick Samson <samson@kirschbaumdevelopment.com>
2022-01-20 13:27:01 -0500

    Improve rounding and add to multiplyBy

--- src/Money.php
+++ src/Money.php
@@ -13,7 +13,10 @@ class Money
 
     public function __construct(int|float|string $amount = 0)
     {
-        // TODO improve parsing?
+        if (is_float($amount)) {
+            // Correctly parse floats containing exponential numbers.
+            $amount = number_format($amount, self::DEFAULT_SCALE, '.', '');
+        }
         $this->amount = (string) $amount;
     }
 
@@ -67,9 +70,10 @@ class Money
         return $this->newInstance(bcmul($this->amount, $operand->amount));
     }
 
-    public function multiplyBy(string $multiplier): self
+    public function multiplyBy(string $multiplier, bool $round = true): self
     {
-        return $this->newInstance(bcmul($this->amount, $multiplier, self::DEFAULT_SCALE));
+        $result = bcmul($this->amount, $multiplier, self::DEFAULT_SCALE);
+        return $this->newInstance($round ? $this->bcRound($result) : $result);
     }
 
     public function divideByMoney(Money $operand): self
@@ -112,32 +116,37 @@ class Money
         return bccomp($this->amount, 0) < 0;
     }
 
-    public function isEqualTo(Money $operand)
+    public function isEqualTo(Money $operand): bool
     {
         return $this->amount === $operand->amount;
     }
 
-    public function isNotEqualTo(Money $operand)
+    public function isNotEqualTo(Money $operand): bool
     {
         return $this->amount !== $operand->amount;
     }
 
     public function round(int $precision = 0)
     {
-        if (strpos($this->amount, '.') !== false) {
-            if ($this->amount[0] != '-') {
-                return $this->newInstance(bcadd($this->amount, '0.' . str_repeat('0', $precision) . '5', $precision));
-            }
-
-            return $this->newInstance(bcsub($this->amount, '0.' . str_repeat('0', $precision) . '5', $precision));
-        }
-
-        return $this->newInstance($this->amount);
+        return $this->newInstance($this->bcRound($this->amount, $precision));
     }
 
     public function clone(): self
     {
         return clone $this;
+    }
+
+    private static function bcRound(string $amount, int $precision = 0): string
+    {
+        if (strpos($amount, '.') !== false) {
+            if ($amount[0] != '-') {
+                return bcadd($amount, '0.' . str_repeat('0', $precision) . '5', $precision);
+            }
+
+            return bcsub($amount, '0.' . str_repeat('0', $precision) . '5', $precision);
+        }
+
+        return $amount;
     }
 
     private function newInstance(string $amount): self
